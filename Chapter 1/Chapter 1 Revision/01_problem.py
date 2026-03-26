d34cb5d46b32297fe4c5748af3de0f76c05eec78
priyanshusingh055 <hellojesse999@gmail.com>
2026-03-26 17:48:31 +0530

    Add Chapter 1 Revision

---
+++ Chapter 1/Chapter 1 Revision/01_problem.py
@@ -0,0 +1,10 @@
+import random
+
+num =random.randint(1,500) 
+print("random Number:",num) # print random number to enter user choice
+
+num2=random.choice([1,55,60,80,-4])
+
+print("Random Choice:",num2)  # print only choice list number 
+
+print(random.random()) # print 0 and 1 mid range number 
---
+++ Chapter 1/Chapter 1 Revision/02_problem.py
@@ -0,0 +1,10 @@
+import os 
+
+path="."
+
+files =os.listdir(path)
+
+print("File in Directory :")
+
+for file in files:
+    print(file)
