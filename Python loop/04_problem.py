8307428806ebbe88bcaa38353e470914b8419347
priyanshusingh055 <hellojesse999@gmail.com>
2026-03-30 21:25:48 +0530

    pattern printing

---
+++ Python loop/04_problem.py
@@ -0,0 +1,7 @@
+ # Star Print 
+
+
+n = int(input("Enter rows:"))
+
+for i in range(1, n+1):
+    print("*" * i)
---
+++ Python loop/07_problem.py
@@ -0,0 +1,10 @@
+
+n = int(input("Enter rows: "))
+
+# Upper part
+for i in range(1, n+1):
+    print(" "*(n-i) + "*"*(2*i-1))
+
+# Lower part
+for i in range(n-1, 0, -1):
+    print(" "*(n-i) + "*"*(2*i-1))
