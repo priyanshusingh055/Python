f332ae4a7c8669d6c6ff0a985730c3689068e0e1
priyanshusingh055 <hellojesse999@gmail.com>
2026-03-31 22:55:58 +0530

    add self in class

---
+++ Chapter 10/03_self.py
@@ -0,0 +1,14 @@
+
+class emplooy:
+     language = "PY"  # This is a Class Attribute 
+     salary = 3000000
+
+     def getifo(self):
+          print(f"This languarg is {self.language} The Salary is {self.salary}")
+
+
+hello = emplooy()
+hello.language = "JS" # This is a instance attribute
+print(hello.language,hello.salary )
+
+hello.getifo()
