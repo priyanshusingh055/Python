761d44686abd57d62b584be22debcc4c93d3fdb2
priyanshusingh055 <hellojesse999@gmail.com>
2026-03-31 22:43:09 +0530

    Chapter 10

---
+++ Chapter 10/01_class.py
@@ -0,0 +1,8 @@
+
+class employee:
+    name = "Priyanshu"
+    language ="PY"
+    salary = 1200000
+
+priyanshu = employee()
+print(priyanshu.name,priyanshu.language)   
---
+++ Chapter 10/02_instance_vs_class.py
@@ -0,0 +1,9 @@
+
+class emplooy:
+     language = "PY"  # This is a Class Attribute 
+     salary = 3000000
+
+
+hello = emplooy()
+hello.language = "JS" # This is a instance attribute
+print(hello.language,hello.salary )
