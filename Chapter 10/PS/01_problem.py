cc81bc4ea3e4e07b64839e4f1b5fbf69aa6dee74
priyanshusingh055 <hellojesse999@gmail.com>
2026-04-01 12:16:31 +0530

    add Chapter 10 and Practice Set

---
+++ Chapter 10/PS/01_problem.py
@@ -0,0 +1,12 @@
+
+
+class programmer:
+   company = "Microsoft"
+   def __init__(self,name,salary,pin):
+      self.name = name
+      self.salary =salary
+      self.pin = pin
+
+p = programmer("Priyanshu", 1250000, 25403)
+print(p.name,p.salary,p.pin)
+      
---
+++ Chapter 10/PS/02_problem.py
@@ -0,0 +1,19 @@
+
+class Calculator:
+    def __init__(self,n):
+        self.n=n
+
+    def square(self):
+            print(f" This Square is {self.n*self.n}")
+
+    def cube(self):
+            print(f" This Cube is {self.n*self.n}")
+
+    def squareroot(self):
+            print(f" This Squareroot is {self.n**1/2}")
+
+a = Calculator(4)
+a.square()
+a.cube()
+a.squareroot()
+
