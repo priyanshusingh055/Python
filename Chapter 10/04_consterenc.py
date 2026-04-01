e929130515fa1307f261399597227fbfc6602506
priyanshusingh055 <hellojesse999@gmail.com>
2026-04-01 11:57:19 +0530

    Chapter 10

---
+++ Chapter 10/04_consterenc.py
@@ -0,0 +1,22 @@
+
+
+class emplooy:
+     language = "PY"  # This is a Class Attribute 
+     salary = 3000000
+
+     def __init__(self):   # Dunder method this is automatic call 
+          print("I am creating a web")
+
+     def getifo(self):
+          print(f"This languarg is {self.language} The Salary is {self.salary}")
+
+          @ staticmethod
+          def greet():
+               print("Good Morning")
+
+
+hello = emplooy()
+hello.namee ="Priyanshu"
+print(hello.namee,hello.salary)
+rohan = emplooy()
+
