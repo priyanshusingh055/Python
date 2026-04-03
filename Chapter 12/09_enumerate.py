af03dd9ba9774a84fa9dba68c291ccf95d15c147
priyanshusingh055 <hellojesse999@gmail.com>
2026-04-03 23:28:31 +0530

    add chapter 12

---
+++ Chapter 12/09_enumerate.py
@@ -0,0 +1,13 @@
+l = [1,55,88,95,44]
+
+# index =0
+
+# for item in l:
+    
+#     print(f"The iteam number at index {index} is {item}")
+#     index +=1
+
+# Shortcut Method =============
+
+for index ,item in enumerate(l):
+    print(f"The iteam number at index {index} is {item}")
---
+++ Chapter 12/10_list_comp.py
@@ -0,0 +1,10 @@
+
+my_list = [1,4,8,7,9]
+
+# squaredList =[]
+# for item in my_list:
+#     squaredList.append(item*item)
+
+squaredList =[i*i for i in my_list]
+
+print(squaredList)
