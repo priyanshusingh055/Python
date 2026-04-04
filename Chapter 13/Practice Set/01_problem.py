2ea076a7023001f482934a077d8a470ea9756f46
priyanshusingh055 <hellojesse999@gmail.com>
2026-04-04 23:50:55 +0530

    chapter 13

---
+++ Chapter 13/Practice Set/01_problem.py
@@ -0,0 +1,7 @@
+
+name = (input("Enter Name :"))
+marks = int(input("Enter A Marks:"))
+number = int(input("Enter A Number :"))
+
+s = "The Name of Student is {},  his marks are {} ,and phone number is {}".format(name,marks,number)
+print(s)
